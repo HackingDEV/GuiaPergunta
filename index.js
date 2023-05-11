931f3899aa252f5adef3fc8d7e98061ca94654fb
HackingDEV <Hackinginvasion386@gmail.com>
2023-05-11 19:16:38 -0300

    Commit01

--- index.js
+++ index.js
@@ -22,8 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Rotas
 app.get("/", (req, res) => {
-  
-  res.render("index");
+  Pergunta.findAll({ raw: true, order:[
+    ['id','DESC'] //ASC = Crescente || DESC = Decrescente
+  ]}).then(perguntas => {
+    res.render("index", {
+      perguntas: perguntas
+    });
+  });
 });
 
 app.get("/perguntar", (req, res) => {
